ab468872c55cec50f85f42baa1ea6f38bf444264
KZKV274 <kazakovshabdan@gmail.com>
2026-07-09 17:29:21 +0600

    Update cache name and asset paths in sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = 'box-dist-v2';
+const CACHE_NAME = 'box-dist-v3';
 const ASSETS = [
   './index.html',
   './manifest.json',
-  './icon-192.png',
-  './icon-512.png'
+  './icons/icon-192.png',
+  './icons/icon-512.png'
 ];
 
 self.addEventListener('install', (event) => {
